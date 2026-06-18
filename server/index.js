1a1fd53e0e15092d7395af8ccc3c14dadeeeda53
nithin0706 <nithinpr2006@gmail.com>
2026-06-18 22:40:15 +0530

    mount auth routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express=require("express");
 const dotenv=require("dotenv");
 const connectDB=require("./config/db");
+const authRoutes=require("./routes/authRoutes");
 dotenv.config();
 connectDB();
 const app=express();
 app.use(express.json());
-app.get ("/",(req,res)=>{
-    res.send(" API is running...");
-});
+app.use("/api/auth",authRoutes);
+app.get ("/",(req,res)=>{res.send("API is running...");});
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{  console.log(`Server running on port ${PORT}`);});
