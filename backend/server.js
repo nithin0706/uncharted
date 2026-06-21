b64c410a7905078bdf434004a6424800cd53d879
varsha2006 <varshapallippadan06@gmail.com>
2026-06-21 18:13:14 +0530

    Connected booking routes to server

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,20 @@ const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-
 require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 const packageRoutes = require("./routes/packageRoutes");
 const destinationRoutes = require("./routes/destinationRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
+
 console.log("Checking MONGO_URI:", process.env.MONGO_URI);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -26,10 +27,11 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
 // Routes
 app.use("/api/packages", packageRoutes);
-
 app.use("/api/destinations", destinationRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 // Test Route
 app.get("/", (req, res) => {
