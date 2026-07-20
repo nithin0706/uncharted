cc5dfbbc4e9252b9c98f187f284ae7737d74abf6
nithin0706 <nithinpr2006@gmail.com>
2026-07-20 23:59:12 +0530

    Fix auth middleware export

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,24 +1,29 @@
-const jwt=require("jsonwebtoken");
-const authMiddleware=(req,res,next)=>{
-   try{
-     const authHeader=req.headers.authorization;
-    if (!authHeader){return res.status(401).json({message: "No token provided"});
-}
-const token = authHeader.split(" ")[1];
-const decoded = jwt.verify(
-    token,
-    process.env.JWT_SECRET
-);
-req.user = decoded;
-next();}
-catch (error) {
+const jwt = require("jsonwebtoken");
 
-        return res.status(401).json({
-            message: "Invalid token"
-        });
+const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
 
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "No token provided",
+      });
     }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
 };
 
-
-module.exports=authMiddleware;
+module.exports = {
+  protect,
+};
