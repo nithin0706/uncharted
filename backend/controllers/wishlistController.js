2fe70d7cf83e99247b5a17a93c92332b52309972
nithin0706 <nithinpr2006@gmail.com>
2026-07-20 21:16:29 +0530

    Fix wishlist route and controller

---
+++ backend/controllers/wishlistController.js
@@ -0,0 +1,53 @@
+const Wishlist = require("../models/Wishlist");
+
+const addToWishlist = async (req, res) => {
+  try {
+    const wishlist = await Wishlist.create({
+      userId: req.user.id,
+      packageId: req.body.packageId,
+    });
+
+    res.status(201).json({
+      message: "Added to wishlist",
+      wishlist,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getWishlist = async (req, res) => {
+  try {
+    const wishlist = await Wishlist.find({
+      userId: req.user.id,
+    }).populate("packageId");
+
+    res.json(wishlist);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const removeFromWishlist = async (req, res) => {
+  try {
+    await Wishlist.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Removed from wishlist",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  addToWishlist,
+  getWishlist,
+  removeFromWishlist,
+};
